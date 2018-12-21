aa8757f2ec8dfaa0b4a339d72664a08e2ab89ed6
TheBrockstar <00AgentB@gmail.com>
2018-12-21 15:58:07 +0900

    Finish dicts.py

--- src/days-1-2/dicts.py
+++ src/days-1-2/dicts.py
@@ -8,22 +8,38 @@
 
 waypoints = [
     {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
-    }, 
+        "lat": 42,
+        "long": 42,
+        "name":"The Truth"
+    },
     {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
-    }, 
+        "lat": 777,
+        "long": 777,
+        "name": "Heaven"
+    },
     {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
+        "lat": 666,
+        "long": 666,
+        "name": "Hell" 
     }
+
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
 
+for w in waypoints:
+    print("latitude: ", w["lat"], "longitude: ", w["long"], "name: ", w["name"])
+
+print("")
+
 # Add a new waypoint to the list
+newWaypoint = {
+    "lat": 3,
+    "long": 3,
+    "name":"The Triangle"
+}
+
+waypoints.append(newWaypoint)
+
+for w in waypoints:
+    print("latitude: ", w["lat"], "longitude: ", w["long"], "name: ", w["name"])
