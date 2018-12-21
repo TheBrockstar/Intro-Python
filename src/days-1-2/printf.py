77a5c18342a86f6b796d95820ad7ebd83cbc4012
TheBrockstar <00AgentB@gmail.com>
2018-12-21 14:07:53 +0900

    Fix Formatting on printf.py

--- src/days-1-2/printf.py
+++ src/days-1-2/printf.py
@@ -8,8 +8,8 @@ z = "I like turtles!"
 print('x is %d, y is %.2f, z is "%s"' % (x, y, z))
 
 # Use the 'format' string method to print the same thing
-print('x is {0:}, y is {1:.3}, z is "{2:}"'.format(x, y, z)) # Manual
-print('x is {:}, y is {:.3}, z is "{:}"'.format(x, y, z)) # Automatic
+print('x is {0}, y is {1:.3}, z is "{2}"'.format(x, y, z)) # Manual
+print('x is {}, y is {:.3}, z is "{}"'.format(x, y, z)) # Automatic
 
 # Finally, print the same thing using an f-string
 print(f'x is {x}, y is {y:.3}, z is "{z}"')
