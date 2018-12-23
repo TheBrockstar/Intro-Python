68a151bc192c1ec92374c6535a0e4653c77dbf6c
TheBrockstar <00AgentB@gmail.com>
2018-12-23 23:25:32 +0900

    Build/Implement Item class

--- src/days-3-4-adv/adv.py
+++ src/days-3-4-adv/adv.py
@@ -1,6 +1,13 @@
 from room import Room
 from player import Player
+from item import Item
 import textwrap
+
+# Declare all the items
+
+item = {
+    'key1': Item("Rusted Iron Key", "A heavy iron key spotted with red blooms of rust.")
+}
 
 # Declare all the rooms
 
@@ -23,6 +30,11 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+# Link items to rooms
+
+room['outside'].items = {
+        item['key1'].name: item['key1']
+    }
 
 # Link rooms together
 
@@ -53,8 +65,8 @@ player = Player("Chosen One", room['outside'])
 #
 # If the user enters "q", quit the game.
 
-command = 'begin'
-while command != 'q':
+cinput = ['begin']
+while cinput[0] != 'q':
     # Title
     print('\n--------------------------------------------------')
     print('============== THE CAVERN OF WONDER ==============')
@@ -68,44 +80,95 @@ while command != 'q':
     location = locationWrapper.wrap(player.room.description)
     for line in location:
         print(line)
+    for item in player.room.items:
+        print(player.room.items[item].name)
+
+    # Command Function Container
+    def command(command):
+
+        # Define Command Action
+        caction = command[0]
+
+        # If One Exists, Define Command Object
+        if len(command) == 1:
+            cobject = ''
+        else:
+            cobject = command[1]
+
+        # -- Command Handling
+
+        ## - Helper Functions
+
+        ### Press Enter to continue...
+        def entocont():
+            input('\nPress Enter to continue...')
+
+        ## - Help
+        if caction == 'h' or caction == 'help':
+            print("""Lost, traveler?
+
+Commands are made of two words: an action, and an object.
+- The action is what you want to do. 
+- The object, which is sometimes optional, is what the action should 
+be done to.
+    
+i.e. search room <= 'search' is the action, and 'room' is the object.
+Alternatively, this could be written as 'search' because the object 
+is sometimes optional.""")
+            entocont()
+
+        ## - Movement
+        
+        ### Movement Error
+        def nodirection():
+            print('\nYou are unable to see a way to move in that direction!')
+            entocont()
+
+        ### North
+        if caction == 'n':
+            if hasattr(player.room, 'n_to'):
+                player.room = player.room.n_to
+            else:
+                nodirection()
+            return
+        
+        ### South
+        if caction == 's':
+            if hasattr(player.room, 's_to'):
+                player.room = player.room.s_to
+            else:
+                nodirection()
+            return
+        ### East
+        if caction == 'e':
+            if hasattr(player.room, 'e_to'):
+                player.room = player.room.e_to
+            else:
+                nodirection()
+
+        ### West
+        if caction == 'w' or ((caction == 'go' or caction == 'walk') and (cobject == 'w' or cobject == 'west')):
+            if hasattr(player.room, 'w_to'):
+                player.room = player.room.w_to
+            else:
+                nodirection()
+            return
+
+        ## - Items
+        if caction == 'get':
+            if cobject in player.room.items:
+                print('You got it!')
+            else:
+                print('You cannot have it.')
+            entocont()
+
+
+            
 
     # User Input
     print('__________________________________________________')
-    command = input('\nEnter a command: ')
+    cinput = input('\nEnter a command: => ').split(" ", 1)
 
-    # -- Commands
 
-    ## - Movement
 
-    ### Movement Error
-    def nodirection():
-        print('\nYou are unable to see a way to move in that direction!')
-        input()
-
-    ### North
-    if command == 'n':
-        if hasattr(player.room, 'n_to'):
-            player.room = player.room.n_to
-        else:
-            nodirection()
-    
-    ### South
-    if command == 's':
-        if hasattr(player.room, 's_to'):
-            player.room = player.room.s_to
-        else:
-            nodirection()
-
-    ### East
-    if command == 'e':
-        if hasattr(player.room, 'e_to'):
-            player.room = player.room.e_to
-        else:
-            nodirection()
-
-    ### West
-    if command == 'w':
-        if hasattr(player.room, 'w_to'):
-            player.room = player.room.w_to
-        else:
-            nodirection()
+    command(cinput)
